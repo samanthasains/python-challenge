d9088ab73262a3e0bbf675be73efd101677ee448
samanthasains <samanthasains@gmail.com>
2020-09-30 17:08:53 -0500

    added instructions

---
+++ PyPoll/main.py
@@ -0,0 +1,15 @@
+#In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
+
+#The dataset is composed of three columns: Voter ID, County, and Candidate.
+
+#Analyze the votes and calculates each of the following:
+
+#The total number of votes cast
+
+#A complete list of candidates who received votes
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote.
