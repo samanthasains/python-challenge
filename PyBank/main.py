459af018d6bab86fbb7cece4d8ef8bb89cbeb9a6
samanthasains <samanthasains@gmail.com>
2020-09-30 16:36:54 -0500

    Added lines 21-32 for average changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,14 +21,15 @@ with open('C:/Users/saman/Documents/NU_Data_Science_Bootcamp/python-challenge/Py
 
 # Find the total number of months included in the dataset - count of items
 totalMonths = len(date)
-print(totalMonths)
 
 #Find the net total amount of "Profit/Losses" over the entire period - sum of the values in [1]
-totalAmount = sum(profitLoss)                        
-print(totalAmount)
+totalAmount = sum(profitLoss)                    
 
 # Find the average of the changes in "Profit/Losses" over the entire period - average of the values in [1]
-averageChange = netTotal/totalMonths
+revenueChange = []
+for x in range(0,totalMonths-1):
+    revenueChange.append(profitLoss[x+1]-profitLoss[x])
+averageChange=(sum(revenueChange)/(totalMonths-1))
 
 # Find the greatest increase in profits (date and amount) over the entire period - maximum value
 For i in profitLoss:
