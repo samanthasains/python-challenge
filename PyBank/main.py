e5a79e654eddfc6747e8c2aebf0d09a0b9c9891a
samanthasains <samanthasains@gmail.com>
2020-09-30 20:07:01 -0500

    added change counter to for loop to calculate greatest increase and decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,8 @@
 # Lists to store data
 date = []
 profitLoss = []
+greatestIncrease = ['',0]
+greatestDecrease = ['', 1000000]
 
 # Path to collect data from the Resources folder
 import csv
@@ -10,6 +12,12 @@ with open('C:/Users/saman/Documents/NU_Data_Science_Bootcamp/python-challenge/Py
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',') 
     header = next(csvreader)
+    firstRow = next(csvreader)
+
+    date.append(firstRow[0])    
+    profitLoss.append(int(firstRow[1]))
+    previousRow=int(firstRow[1])
+
     if header != None:
         for row in csvreader:
         
@@ -19,33 +27,32 @@ with open('C:/Users/saman/Documents/NU_Data_Science_Bootcamp/python-challenge/Py
             #Add profit/loss
             profitLoss.append(int(row[1])) 
 
-# Find the total number of months included in the dataset - count of items
+            change=int(row[1])-previousRow
+            previousRow=int(row[1])
+
+            if change>greatestIncrease[1]:
+                greatestIncrease[0]=row[0]
+                greatestIncrease[1]=change
+            
+            if change<greatestDecrease[1]:
+                greatestDecrease[0]=row[0]
+                greatestDecrease[1]=change
+
+# # Find the total number of months included in the dataset - count of items
 totalMonths = len(date)
 
-#Find the net total amount of "Profit/Losses" over the entire period - sum of the values in [1]
+# #Find the net total amount of "Profit/Losses" over the entire period - sum of the values in [1]
 totalAmount = sum(profitLoss)                    
 
-# Find the average of the changes in "Profit/Losses" over the entire period - average of the values in [1]
+# # Find the average of the changes in "Profit/Losses" over the entire period - average of the values in [1]
 revenueChange = []
 for x in range(0,totalMonths-1):
     revenueChange.append(profitLoss[x+1]-profitLoss[x])
 averageChange=(sum(revenueChange)/(totalMonths-1))
 
-# Find the greatest increase in profits (date and amount) over the entire period - maximum value
-For i in profitLoss:
-    if i > i-1, then :
-        greatestIncrease = i
-        increaseDate = date[i]
-
-#Find greatest decrease in losses (date and amount) over the entire period - minimum value
-For i in profitLoss:
-    if i < i-1, then:
-        greatestDecrease = i
-        decreaseDate = date[i]
-
-# Print the analysis to the terminal and export a text file with the results
-print('Financial Analysis\n----------------')
-print('Total Months: ' + str(totalMonths))
-print('Average Change: $' +str(averageChange))
-print('Greatest Increase in Profits: ' + increaseDate' ' + greatestIncrease)
-print('Greatest Descrease in Profits: ' + decreaseDate' ' + greatestDecrease)
+# # # Print the analysis to the terminal and export a text file with the results
+# print('Financial Analysis\n----------------')
+# print({'Total months' & {totalMonths})
+# print('Average Change: $' + averageChange)
+# print('Greatest Increase in Profits: ' + greatestIncrease)
+# print('Greatest Descrease in Profits: ' + greatestDecrease)
