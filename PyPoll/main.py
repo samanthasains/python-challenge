16781c0aa10c882defa96ef34a1bbca39242b397
samanthasains <samanthasains@gmail.com>
2020-10-02 21:21:30 -0500

    Added counters for candidate votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,9 @@
 
 #Analyze the votes and calculates each of the following:
 
-#The total number of votes cast
+#The total number of votes cast (done)
 
-#A complete list of candidates who received votes
+#A complete list of candidates who received votes (done)
 
 #The percentage of votes each candidate won
 
@@ -35,14 +35,34 @@ with open('C:/Users/saman/Documents/NU_Data_Science_Bootcamp/python-challenge/Py
             #Add candidates
             candidate.append(row[2])
     
-#Read data to python lists from Voter ID row[0] and Candidate (row[2])
-
 #Count of items in list totalVotes=(len)
+totalVotes=len(candidate)
 
 #Generate unique entries from list of candidates
+candidate_names=set(candidate)
+candidate_names=list(candidate_names)
 
-#Somehow create dictionary of voter ID associated with each candidate?
+# Set vote counters for candidates
+candidateOne=0
+candidateTwo=0
+candidateThree=0
+candidateFour=0
+
+#Generate vote numbers for each candidate
+for name in candidate:
+    if name == candidate_names[0]:
+        candidateOne = candidateOne + 1
+    if name == candidate_names[1]:
+        candidateTwo=candidateTwo+1
+    if name == candidate_names[2]:
+        candidateThree=candidateThree+1
+    if name==candidate_names[3]:
+        candidateFour=candidateFour+1
 
 #candidatePercent = len(each_candidate)/totalVotes
 
-#
+#Print results to text file
+print('Elections Results\n----------------')
+print('Total Votes : ' + str(totalVotes))
+print('\n----------------')
+print(str(candidate_names[0])": " str(candidateOne/totalVotes) + ' ('str(candidateOne):')')
